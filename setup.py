6c00febe0a12283a3a7fde79f6f580afffa2c45a
Amos Decker <amdecker9@gmail.com>
2025-05-02 14:33:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
-print("running setup.py")
+from setuptools import setup, find_packages
 setup(
     name="pytrajlib",
     version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
