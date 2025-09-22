4a109388adb04f7c3d886cc55689e3e44945c3f8
Amos Decker <amdecker9@gmail.com>
2025-09-22 12:25:05 -0400

    update atm plot

--- src/custom_scripts/atmplot.py
+++ src/custom_scripts/atmplot.py
@@ -1,9 +1,25 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import scienceplots
 
-plt.style.use(['science'])
-plt.style.use(['no-latex'])
+plt.style.use(["science"])
+plt.style.use(["no-latex"])
+
+plt.figure(figsize=(5, 5))
+ax = plt.gca()
+
+params = {
+    "axes.labelsize": 8,
+    "font.size": 8,
+    "font.family": "serif",
+    "legend.fontsize": 8,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+    # 'text.usetex': True,
+}
+plt.rcParams.update(params)
+colors = plt.cm.viridis(np.linspace(0, 1, 7))
+
 
 # Import mean atmosphere
 mean_atm = np.loadtxt("src/pytrajlib/config/mean_atm.txt")
@@ -11,68 +27,68 @@ mean_atm = np.loadtxt("src/pytrajlib/config/mean_atm.txt")
 # Import batch of atmospheric profiles
 atm_profiles = np.loadtxt("src/pytrajlib/config/atmprofiles.txt")
 
-# Calculate the standard deviation of the atmospheric profiles
-# First, reformat the data by iterating over the profiles
-# Get the number of distinct values of atm_profiles[:, 0]
 profile_numbers = np.unique(atm_profiles[:, 0])
 num_profiles = len(profile_numbers)
 altitudes = np.unique(atm_profiles[:, 1])
 num_altitudes = len(altitudes)
 
 std_values = np.zeros((num_altitudes))  # Altitude, mean density, std density
-for i in range(num_profiles):
-    density_values = atm_profiles[atm_profiles[:, 1] == i, 2]
-    # Calculate the mean and standard deviation of the density values
-    mean_density = np.mean(density_values)
-    std_density = np.std(density_values)
-    std_values[i] = std_density
+density_values = []
+wind_values = []
+for i in range(num_altitudes):
+    # values at altitude i for all profiles
+    density = atm_profiles[atm_profiles[:, 1] == i, 2]
+    wind = np.sqrt(
+        np.square(atm_profiles[atm_profiles[:, 1] == i, 3])
+        + np.square(atm_profiles[atm_profiles[:, 1] == i, 4])
+        + np.square(atm_profiles[atm_profiles[:, 1] == i, 5])
+    )
 
-plt.figure(figsize=(5,5))
-ax = plt.gca()
+    density_values.append(density)
+    wind_values.append(wind)
 
-params = {
-    'axes.labelsize': 8,
-    'font.size': 8,
-    'font.family': 'serif',
-    'legend.fontsize': 8,
-    'xtick.labelsize': 10,
-    'ytick.labelsize': 10,
-    # 'text.usetex': True,
-}
+density_values = np.array(density_values)
+wind_values = np.array(wind_values)
 
-plt.rcParams.update(params)
-colors = plt.cm.viridis(np.linspace(0, 1, 7))
+mean_density = np.mean(density_values, axis=1)
+std_density = np.std(density_values, axis=1)
+mean_wind = np.mean(wind_values, axis=1)
+std_wind = np.std(wind_values, axis=1)
+
+altitude = mean_atm[:, 0]
+
 
 # Plot mean density
-plt.plot(mean_atm[:, 1], mean_atm[:, 0], label="Mean Density")
-plt.fill_betweenx(mean_atm[:, 0], mean_atm[:, 1]-std_values, mean_atm[:, 1]+std_values, alpha=0.2, label="Std Dev")
+plt.plot(mean_density, altitude, label="Mean Density")
+plt.fill_betweenx(
+    altitude,
+    mean_density - std_density,
+    mean_density + std_density,
+    alpha=0.2,
+    label="Std Dev",
+)
 plt.xscale("log")
-plt.xlabel("Pressure (Pa)")
+plt.xlabel("Density (kg / m^3)")
 plt.ylabel("Altitude (km)")
-plt.title("Mean Atmospheric Pressure")
-# Add legend
+plt.title("Mean Atmospheric Density")
 plt.legend()
 plt.tight_layout()
-plt.savefig("output/mean_density.jpg", dpi=1000)
+density_path = "output/mean_density.jpg"
+plt.savefig(density_path, dpi=1000)
 plt.close()
 
-# Now, repeat this for the velocity profile
-std_values = np.zeros((num_altitudes))  # Altitude, mean density, std density
-for i in range(num_profiles):
-    wind_values = np.sqrt(np.square(atm_profiles[atm_profiles[:, 1] == i, 3]) + np.square(atm_profiles[atm_profiles[:, 1] == i, 4]) + np.square(atm_profiles[atm_profiles[:, 1] == i, 5]))
-
-    mean_wind = np.mean(wind_values)
-    std_wind = np.std(wind_values)
-    std_values[i] = std_wind
-
 # Plot mean atmosphere
-plt.plot(mean_atm[:, 1], mean_atm[:, 0], label="Mean Windspeed")
-plt.fill_betweenx(mean_atm[:, 0], mean_atm[:, 1]-std_values, mean_atm[:, 1]+std_values, alpha=0.2, label="Std Dev")
+plt.plot(mean_wind, altitude, label="Mean Windspeed")
+plt.fill_betweenx(
+    altitude, mean_wind - std_wind, mean_wind + std_wind, alpha=0.2, label="Std Dev"
+)
 plt.xlabel("Windspeed (m/s)")
 plt.ylabel("Altitude (km)")
 plt.title("Mean Atmospheric Windspeed")
-# Add legend
 plt.legend()
 plt.tight_layout()
-plt.savefig("output/mean_winds.jpg", dpi=1000)
+wind_path = "output/mean_winds.jpg"
+plt.savefig(wind_path, dpi=1000)
 plt.close()
+
+print(f"Saved to {density_path} and {wind_path}")
