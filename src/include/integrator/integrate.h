027fdc8495a31cdcd4c78213ead58cf0c603a756
Amos Decker <amdecker9@gmail.com>
2026-01-30 16:40:27 +0800

    Use Milstien/Euler-Maruyama to update gyro error

--- src/include/integrator/integrate.h
+++ src/include/integrator/integrate.h
@@ -2,6 +2,7 @@
 #define INTEGRATE_H
 
 #include "models/state.h"
+#include "rng/rng.h"
 #include "utils/derivatives.h"
 #include <math.h>
 
@@ -47,12 +48,14 @@ quaternion quaternion_update(state current_state, state state_deriv_drift,
 
 /**
  * Use the Velocity Verlet method for integrating the deterministic drift
- * component and the Milstein (reduces to Euler-Maruyama for diffusion term
+ * component, the Milstein (reduces to Euler-Maruyama for diffusion term
  * because the noise is additive) method for integrating the stochastic
- * diffusion component.
+ * gyro error, and an exact quaternion updator method based on the angular
+ * velocity for updating the quaternion.
  *
  * @param current_state: initial state object
- * @param drift: function for deterministic term
+ * @param drift: function for deterministic terms
+ * @param diffusion: function for stochastic terms
  * @param args: additional arguments for the drift and diffusion functions
  * @param max_steps: maximum number of steps the integrator will take
  * @param dt: time step
@@ -60,20 +63,21 @@ quaternion quaternion_update(state current_state, state state_deriv_drift,
  * continue and 0 if it should stop
  * @return: final state after integration
  */
-state integrate(state current_state, DerivFunction get_deriv,
-                integrator_args args, int max_steps, double dt,
-                EventFunction event) {
+state integrate(state current_state, DerivFunction drift,
+                DerivFunction diffusion, integrator_args args, int max_steps,
+                double dt, EventFunction event) {
   double t = 0;
   int step_counter = 0;
-  state state_deriv = get_deriv(t, &current_state, &args);
+  state drift_deriv = drift(t, &current_state, &args);
   t += dt;
 
   while (dummy_event(t, &current_state, &args) && (step_counter < max_steps)) {
-    cartvec d_a_lift_dt = state_deriv.a_lift;
-    cartvec d_a_lift_avail_dt = state_deriv.a_lift_avail;
+    // Velocity Verlet for integrating the position and velocity updates
+    cartvec d_a_lift_dt = drift_deriv.a_lift;
+    cartvec d_a_lift_avail_dt = drift_deriv.a_lift_avail;
 
     // Update position
-    cartvec acc = state_deriv.velocity;
+    cartvec acc = drift_deriv.velocity;
     current_state.position.x +=
         current_state.velocity.x * dt + 0.5 * dt * dt * acc.x;
     current_state.position.y +=
@@ -87,32 +91,41 @@ state integrate(state current_state, DerivFunction get_deriv,
     current_state.velocity.z += 0.5 * dt * acc.z;
 
     // Get acceleration & other derivatives
-    state_deriv = get_deriv(t, &current_state, &args);
+    drift_deriv = drift(t, &current_state, &args);
 
     // Update velocity
-    current_state.velocity.x += state_deriv.velocity.x * 0.5 * dt;
-    current_state.velocity.y += state_deriv.velocity.y * 0.5 * dt;
-    current_state.velocity.z += state_deriv.velocity.z * 0.5 * dt;
+    current_state.velocity.x += drift_deriv.velocity.x * 0.5 * dt;
+    current_state.velocity.y += drift_deriv.velocity.y * 0.5 * dt;
+    current_state.velocity.z += drift_deriv.velocity.z * 0.5 * dt;
 
     // Update lift acceleration
-    current_state.a_lift.x += (d_a_lift_dt.x + state_deriv.a_lift.x) * 0.5 * dt;
-    current_state.a_lift.y += (d_a_lift_dt.y + state_deriv.a_lift.y) * 0.5 * dt;
-    current_state.a_lift.z += (d_a_lift_dt.z + state_deriv.a_lift.z) * 0.5 * dt;
+    current_state.a_lift.x += (d_a_lift_dt.x + drift_deriv.a_lift.x) * 0.5 * dt;
+    current_state.a_lift.y += (d_a_lift_dt.y + drift_deriv.a_lift.y) * 0.5 * dt;
+    current_state.a_lift.z += (d_a_lift_dt.z + drift_deriv.a_lift.z) * 0.5 * dt;
 
     // Update available lift acceleration
     current_state.a_lift_avail.x +=
-        (d_a_lift_avail_dt.x + state_deriv.a_lift_avail.x) * 0.5 * dt;
+        (d_a_lift_avail_dt.x + drift_deriv.a_lift_avail.x) * 0.5 * dt;
     current_state.a_lift_avail.y +=
-        (d_a_lift_avail_dt.y + state_deriv.a_lift_avail.y) * 0.5 * dt;
+        (d_a_lift_avail_dt.y + drift_deriv.a_lift_avail.y) * 0.5 * dt;
     current_state.a_lift_avail.z +=
-        (d_a_lift_avail_dt.z + state_deriv.a_lift_avail.z) * 0.5 * dt;
+        (d_a_lift_avail_dt.z + drift_deriv.a_lift_avail.z) * 0.5 * dt;
 
-    // Update gyro noise
-    // TODO
+    // Milstein (equivalent to Euler-Maruyama with additive noise) for
+    // integrating the gyro error
+    state diffusion_deriv = diffusion(t, &current_state, &args);
 
-    // Update quaternion
+    // Update gyro noise drift (deterministic) component
+    current_state.gyro_error.lat += drift_deriv.gyro_error.lat * dt;
+    current_state.gyro_error.lon += drift_deriv.gyro_error.lon * dt;
+    // Update gyro noise diffusion (stochastic) component
+    double dW[2] = {ran_gaussian(sqrt(dt)), ran_gaussian(sqrt(dt))};
+    current_state.gyro_error.lat += diffusion_deriv.gyro_error.lat * dW[0];
+    current_state.gyro_error.lon += diffusion_deriv.gyro_error.lon * dW[1];
+
+    // Update quaternion based on angular velocity
     current_state.quaternion =
-        quaternion_update(current_state, state_deriv, dt);
+        quaternion_update(current_state, drift_deriv, dt);
 
     t += dt;
     step_counter++;
