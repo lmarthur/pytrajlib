fca218b4fe8ace70a4843ec1e555848de2795ac1
Amos Decker <amdecker9@gmail.com>
2026-06-08 10:05:21 -0400

    use macos-latest for build wheels

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pytrajlib",
-    version="1.0.0-alpha.10",
+    version="1.0.0-alpha.11",
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
