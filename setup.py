36ec81bc744d695cb1f0cbf18245d00d0cb3c3cf
Amos Decker <amdecker9@gmail.com>
2026-06-08 14:14:41 -0400

    Build for python 3.13 and 3.14

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pytrajlib",
-    version="1.0.0-alpha.13",
+    version="1.0.0-alpha.14",
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
