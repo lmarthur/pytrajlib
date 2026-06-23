f7663b3edd9a89d6ce557807bf954f70a124a835
Amos Decker <amdecker9@gmail.com>
2026-06-23 16:48:54 -0400

    Optimize boost & maneuvering params

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pytrajlib",
-    version="1.0.0-alpha.23",
+    version="1.0.0-alpha.24",
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
