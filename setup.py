cf12456804805c2f57d18f41a8616382f3cf79fe
Amos Decker <amdecker9@gmail.com>
2026-06-23 14:21:36 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pytrajlib",
-    version="1.0.0-alpha.22",
+    version="1.0.0-alpha.23",
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
