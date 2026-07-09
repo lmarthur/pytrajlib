4d30b51845e7b2a94ff8f03c5d8c021b80b9ffd5
Amos Decker <amdecker9@gmail.com>
2026-07-09 11:17:50 -0400

    Include atmprofiles csv

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pytrajlib",
-    version="1.0.0-alpha.28",
+    version="1.0.0-alpha.29",
     cffi_modules=["src/pytrajlib/build.py:ffibuilder"],
 )
